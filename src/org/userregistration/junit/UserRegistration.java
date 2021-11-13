f083e7c14a9fa52ee80f89105ef4a78d3d31af62
bolishettianjali <bolishettianjali@gamil.com>
2021-11-13 14:19:25 +0530

    Added:Welcome Message

---
+++ src/org/userregistration/junit/UserRegistration.java
@@ -0,0 +1,7 @@
+package org.userregistration.junit;
+
+public class UserRegistration {
+    public static void main(String[] args){
+        System.out.println("Welcome to User Registration problem");
+    }
+}
